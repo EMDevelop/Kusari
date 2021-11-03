3e0a89b107e22d2a6be315147494dadfd0dea121
EMDevelop <edwardmartin.inbox@gmail.com>
2021-11-03 19:11:55 +0000

    navbar no longer absolute, style setup for wallet balance cards

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,38 @@
 import './styles/App.scss';
 import Navbar from './components/Navbar/Navbar';
 import axios from 'axios';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [address, setAddress] = useState('');
 
-  const [address, setAddress] = useState('')
-
-  const handleInputChange = (e) => { 
-    setAddress(e)
-  }
-  const handleButtonClick = async () => { 
+  const handleInputChange = (e) => {
+    setAddress(e);
+  };
+  const handleButtonClick = async () => {
     try {
-      const response = await axios.get(`/ethereum/wallet-balance/`)
-      console.log(response)
+      const response = await axios.get(`/ethereum/wallet-balance/`);
+      console.log(response);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
-
+  };
 
   return (
-    <>
-      {/* <Navbar /> */}
-      <input type="text" placeholder="address" onChange={(e)=>handleInputChange(e.target.value)}/>
-      <button onClick={()=> handleButtonClick()}> click </button>
-    </>
+    <div className="app">
+      <Navbar />
+      <main>
+        <div className="address-input-form">
+          <input
+            type="text"
+            placeholder="address"
+            onChange={(e) => handleInputChange(e.target.value)}
+          />
+          <button onClick={() => handleButtonClick()}> click </button>
+        </div>
+        <div className="token-list"></div>
+      </main>
+    </div>
   );
 }
 
