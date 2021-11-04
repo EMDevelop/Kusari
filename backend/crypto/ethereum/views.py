c9d4b16188b01da04b300af782eedbe12dfebd1d
Mabon ap Gwyn <mabonapgwyn@hotmail.co.uk>
2021-11-04 13:03:14 +0000

    appended usdperunit to array of dictionaries

--- backend/crypto/ethereum/views.py
+++ backend/crypto/ethereum/views.py
@@ -15,8 +15,10 @@ import os
 def create_decimal_from_number(number, decimals):
     return float(number) / pow(10, int(decimals))
 
-def get_wallet_balance(request, address):
 
+
+
+def get_moralis_erc20(address):
     url = f"https://deep-index.moralis.io/api/v2/{address}/erc20?chain=eth"
         
     headers = {
@@ -34,28 +36,65 @@ def get_wallet_balance(request, address):
         token_dict['name'] = token['name']
         token_list.append(token_dict)
 
+    return token_list
 
-    return JsonResponse(token_list,safe=False)
+
+# [{'token': 'BONDLY', 'balance': 993.3971165225804, 'name': 'Bondly Token'}, {'token': 'BSJ', 'balance': 50.0, 'name': 'BASENJI'}]
+def get_list_of_symbols(array_of_token_dictionaries):
+    array_of_symbols = []
+    separator = ','
+    for token in array_of_token_dictionaries:
+        array_of_symbols.append(token['token'])
+    return separator.join(array_of_symbols)
+
+# https://min-api.cryptocompare.com/documentation?key=Price&cat=multipleSymbolsPriceEndpoint
+# https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH&tsyms=USD
+def get_cryptocompare_token_price_by_id(token_data):
+    print('------')
+    print('------')
+    print(token_data)
+    print('------')
+    print('------')
+    # print(token_data['token'])
+    # pass in token array for getting prices using cryptocompare    
+    # url = "https://min-api.cryptocompare.com/data/price?fsym=" + token + "&tsyms=USD,EUR,GBP&api_key=" + os.environ['CRYPTOCOMPARE_API_KEY']
+    url = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=" + get_list_of_symbols(token_data) + "&tsyms=USD"
+    response = requests.request("GET", url)
+    token_prices = json.loads(response.text)
+    print("&&&&&&&")      
+    print(token_prices)
+    print(token_data)
+    # token_wallet_prices = {**token_data, **token_prices}
+    for token in token_data:
+        try: 
+            token['USDperUnit'] = token_prices[token['token']]['USD']
+        except:
+            token['USDperUnit'] = "N/A"
+    
+    print(token_data)
+    print('------')
+    # print(token_wallet_prices)
+
+    return token_wallet_prices
+
+# def get_tokens_in_wallet():
+#     # get the tokens in the wallet, pass the Symbol from token_list to token prices to add the prices to the dict wallet
+#     token_list = get_tokens()
+#     wallet = []
+#     for token in token_list:         
+#         wallet.append(token_prices(token))     
+    
+#     return wallet
 
 
-#     def token_prices(self,token_data):
-#         # pass in token array for getting prices using cryptocompare
-#         token = token_data['token']         
-#         url = "https://min-api.cryptocompare.com/data/price?fsym=" + token + "&tsyms=USD,EUR,GBP&api_key=" + os.environ['CRYPTOCOMPARE_API_KEY']
-#         response = requests.request("GET", url)
-#         token_prices = json.loads(response.text)        
-#         token_wallet_prices = {**token_data, **token_prices}
-#         print('------')
-#         print(token_wallet_prices)
+def get_wallet_balance(request, address):
 
-#         return token_wallet_prices
+    tokens = get_moralis_erc20(address) #refactor into next line
+    # tokenprices = get_cryptocompare_token_price_by_id(tokens)
+    print(tokenprices)
+    wallet = []
+    for token in tokens:
+        wallet.append(tokenprices(token))
+    
 
-#     def get_tokens_in_wallet(self):
-#         # get the tokens in the wallet, pass the Symbol from token_list to token prices to add the prices to the dict wallet
-#         token_list = self.get_tokens()
-#         wallet = []
-#         for token in token_list:            
-#             wallet.append(self.token_prices(token))     
-        
-#         return wallet
-
+    return JsonResponse(tokens, safe=False)
