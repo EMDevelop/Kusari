c8739e662ca82be4e83763268925102226d365ae
Tom Lee <tomalee2002@gmail.com>
2021-11-04 16:33:21 +0000

    removed 2 useless lines

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -85,8 +85,6 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'investmenttracker',
         'USER': 'tom',
