8aff98f52f197d1a710aae1a8b1a2be214466ef0
Tom Lee <tomalee2002@gmail.com>
2021-11-06 23:03:18 +0000

    adds signup and login functionality for the backend

--- backend/prices/urls.py
+++ backend/prices/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from .views import current_user, UserList
 
 urlpatterns = [
-    path('', views.get_coin_gecko_prices)
+    path('', views.get_coin_gecko_prices),
+    path('current_user/', current_user),
+    path('users/', UserList.as_view())
 ]
