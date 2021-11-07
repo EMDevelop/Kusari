6c173fbf3d829b413048ba60739030c518278f44
Mabon ap Gwyn <mabonapgwyn@hotmail.co.uk>
2021-11-07 12:12:00 +0000

    before pulling on main, 1 test not passing

--- backend/crypto/helper/tests.py
+++ backend/crypto/helper/tests.py
@@ -13,7 +13,7 @@ class Get_Cryptocompare_Token_Price_By_Id_Test(TestCase):
 	@mock.patch('requests.request', side_effect=mocked_requests_get)
 	def test_cryptocompare_USD_is_int(self, mock_get):
 		array_of_tokens = get_cryptocompare_token_price_by_id([{"token": "TEST2", "quantity": 1, "name": "TestCoin"}])
-		self.assertEqual(array_of_tokens, [{"USDperUnit": "N/A", "token": "TEST2", "quantity": 1, "name": "TestCoin"}])
+		self.assertEqual(array_of_tokens, [{"USDperUnit": 5, "token": "TEST2", "quantity": 1, "name": "TestCoin"}])
 
 
 class Get_Token_Current_Value_In_USD_Test(TestCase):
