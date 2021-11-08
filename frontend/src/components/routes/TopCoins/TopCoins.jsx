dab201785841fcbcce8e37f0abc591f4c189b53f
Paul Cooper <paul_cooper_pc@hotmail.com>
2021-11-08 16:51:07 +0000

    Top Coins API url added

--- frontend/src/components/routes/TopCoins/TopCoins.jsx
+++ frontend/src/components/routes/TopCoins/TopCoins.jsx
@@ -1,5 +1,53 @@
 import React from 'react';
 
+class TopCoinsPage extends component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false, 
+    }
+  }
+
+  componentDidMount() {
+
+    fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=GBP&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=24h')
+        .then(res => res.json())
+        .then(json => {
+          this.setState({
+            isLoaded: true,
+            items: json,
+          })
+        });
+  }
+
+  render() {
+
+    var { isLoaded, items } = this.state;
+
+    if (!isLoaded) {
+      return <div>Loading...</div>;
+    }
+
+    else {}
+
+      return (
+        <div className="top-coins-page">
+            <ul>
+              {items.map(item => {
+                  <li key={item.id}>
+                      Coin: {item.coin} | Price: {item.price}
+                  </li>    
+              })};
+            </ul>  
+        </div>
+      );
+    }
+}
+
+
+
 export default function TopCoins() {
   return (
     <div className="top-coins-page">
