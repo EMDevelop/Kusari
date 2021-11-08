822fce40f0ea477b053dc05bdb3dc4123a3acc7a
Paul Cooper <paul_cooper_pc@hotmail.com>
2021-11-08 16:53:57 +0000

    Updated Snackbar file

---
+++ frontend/src/components/snackbar/Snackbar.jsx
@@ -0,0 +1,30 @@
+import React, { useState, forwardRef, useImperativeHandle } from "react";
+import "./_snackbar.scss";
+
+const Snackbar = forwardRef((props, ref) => {
+  const [showSnackbar, setShowSnackbar] = useState(false);
+
+  useImperativeHandle(ref, () => ({
+    show() {
+      setShowSnackbar(true);
+      setTimeout(() => {
+        setShowSnackbar(false);
+      }, 3000);
+    },
+  }));
+  return (
+    <div
+      className="snackbar"
+      id={showSnackbar ? "show" : "hide"}
+      style={{
+        backgroundColor: props.type === "success" ? "#32ecb1" : "#f97068ff",
+        color: props.type === "success" ? "black" : "white",
+      }}
+    >
+      <div className="symbol">
+        {props.type === "success" ? <h1>&#x2713;</h1> : <h1>&#x2613;</h1>}
+      </div>
+      <div className="message">{props.message}</div>
+    </div>
+);
+});
