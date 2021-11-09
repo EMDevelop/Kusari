6cf1c1631e8e613db7e6589349660e654b51d88d
Gianluca <gianluca.ansaldi.1989@gmail.com>
2021-11-09 17:30:36 +0000

    imported decorators and response modules in the multiChain views

--- backend/crypto/multiChain/views.py
+++ backend/crypto/multiChain/views.py
@@ -1,5 +1,7 @@
 from django.http.response import JsonResponse
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
 
