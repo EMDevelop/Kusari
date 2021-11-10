274afd0286fd0494336af83b8794818598e15b0f
Admin <admin@Admins-MacBook-Pro.local>
2021-11-10 20:52:20 +0000

    Top Coins Ranking messed up

--- frontend/src/components/routes/TopCoins/TopCoins.jsx
+++ frontend/src/components/routes/TopCoins/TopCoins.jsx
@@ -1,9 +1,29 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import DataTable from '../../dataTable/DataTable';
+import TokenInformation from '../../../tokenInformation/TokenInformation';
 import axios from 'axios';
 
 export default function TopCoins() {
   const [items, setItems] = useState([]);
   const [isLoaded, setIsLoaded] = useState([]);
+
+  const navigate = useNavigate();
+  const [isUSDperUnit, setIsUSDperUnit] = useState([])
+  
+  function SearchWalletBalance() {
+  // const [address, setAddress] = useState(undefined);
+  // const [walletDetails, setWalletDetails] = useState([]);
+  const [walletType, setwalletType] = useState(undefined);
+  const [fetchingAddressInfo, setFetchingAddressInfo] = useState(false);
+
+  const { address, setAddress, walletDetails, setWalletDetails } =
+    useContext(GlobalContext);
+
+  // Every time someone types in the input, the `address` state is updated
+  const handleInputChange = (e) => {
+    setAddress(e);
+  };
 
   useEffect(() => {
     try {
@@ -46,4 +66,34 @@ export default function TopCoins() {
   ) : (
     <div>Loading...</div>
   );
+
+//Ranking Top 100 Coins
+useEffect(() => {
+    filterRowData('USDperUnit');
+  }, [props.rowData]);
+
+
+render (); {
+  const {USDperUnit} = this.state
+  const sorted = usDperUnit.sort( (a, b) => {
+    const isReversed = (sortType === 'desc') ? 1 : -1;
+    return isReversed * a.name.localeCompare(b.name)
+  })
+}
+
+//Click into individual coin performance data
+<Link
+  to={(
+    pathname: '../../../tokenInformation/TokenInformation',
+    state: { datarow, token, name, USDperUnit }
+  )}
+>
+
+render() {
+  const { state } = this.props.const location
+  return {
+
+  }
+    },
+  })
 }
