77ddb5c3aa6888bd26122147d7314ca680863213
EMDevelop <edwardmartin.inbox@gmail.com>
2021-11-10 18:35:53 +0000

    refactored Datatable into smaller components within the DataTableclass

--- frontend/src/components/routes/Portfolio/Portfolio.jsx
+++ frontend/src/components/routes/Portfolio/Portfolio.jsx
@@ -1,14 +1,48 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
+import DataTable from '../../dataTable/DataTable';
+import { GlobalContext } from '../../../context/globalContext';
+import axios from 'axios';
 
 export default function Portfolio() {
+  const { userID } = useContext(GlobalContext);
+  const [portfolioTokens, setPortfolioTokens] = useState(undefined);
+
   useEffect(() => {
-    // Request to the backend so that we can see all of the current users portfolio
-    'multi/get-prices/:username';
+    async function fetchData() {
+      try {
+        const response = await axios.get(`multi/user-portfolio/${userID}/`);
+        console.log(response.data[0].content);
+        setPortfolioTokens(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
   }, []);
 
   return (
     <div className="portfolio-page">
       <h1>My Portfolio</h1>
+      <div>
+        <p>This is going to be 2x dropdowns</p>
+      </div>
+      <div className="portfolio-table">
+        <DataTable
+          headers={[
+            // props.headers
+            'Type',
+            'Address',
+            '',
+            'Symbol',
+            'Token Name',
+            'Quantity',
+            'Price',
+            'Current Value',
+          ]}
+          //rowData={portfolioTokens} // props.rowData
+          label="portfolio" // props.label
+        />
+      </div>
     </div>
   );
 }
