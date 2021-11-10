72e305cb4a23225b18f1f6ba8a8e03da610136d8
EMDevelop <edwardmartin.inbox@gmail.com>
2021-11-10 21:21:54 +0000

    moved the API call into the backend for the Top Tokens

--- frontend/src/components/routes/TopCoins/TopCoins.jsx
+++ frontend/src/components/routes/TopCoins/TopCoins.jsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import DataTable from '../../dataTable/DataTable';
-import TokenInformation from '../../../tokenInformation/TokenInformation';
+import { GlobalContext } from '../../../context/globalContext';
+// import TokenInformation from '../../../TokenInformation/TokenInformation';
 import axios from 'axios';
 
 export default function TopCoins() {
@@ -9,28 +10,12 @@ export default function TopCoins() {
   const [isLoaded, setIsLoaded] = useState([]);
 
   const navigate = useNavigate();
-  const [isUSDperUnit, setIsUSDperUnit] = useState([])
-  
-  function SearchWalletBalance() {
-  // const [address, setAddress] = useState(undefined);
-  // const [walletDetails, setWalletDetails] = useState([]);
-  const [walletType, setwalletType] = useState(undefined);
-  const [fetchingAddressInfo, setFetchingAddressInfo] = useState(false);
-
-  const { address, setAddress, walletDetails, setWalletDetails } =
-    useContext(GlobalContext);
-
-  // Every time someone types in the input, the `address` state is updated
-  const handleInputChange = (e) => {
-    setAddress(e);
-  };
 
   useEffect(() => {
     try {
       async function fetchData() {
-        const response = await axios.get(
-          'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
-        );
+        const response = await axios.get(`prices/top-coins/`);
+        console.log(response.data);
         setItems(response.data);
         setIsLoaded(true);
       }
@@ -43,21 +28,19 @@ export default function TopCoins() {
   return isLoaded ? (
     <div className="top-coins-page">
       <h1>Top Coins</h1>
-      <div> middle section</div>
       <div>
         <div className="top-coins-page">
           <ul>
-            {items.map((item) => {
+            {/* {items.map((item) => {
               return (
                 <li key={item.id}>
-                  {/* <img src={item.image} alt="" /> */}
                   <h4>
                     Currency: {item.name} | Current Price: ${item.current_price}{' '}
                     | Market Cap: ${item.market_cap}
                   </h4>
                 </li>
               );
-            })}
+            })} */}
             ;
           </ul>
         </div>
@@ -66,34 +49,4 @@ export default function TopCoins() {
   ) : (
     <div>Loading...</div>
   );
-
-//Ranking Top 100 Coins
-useEffect(() => {
-    filterRowData('USDperUnit');
-  }, [props.rowData]);
-
-
-render (); {
-  const {USDperUnit} = this.state
-  const sorted = usDperUnit.sort( (a, b) => {
-    const isReversed = (sortType === 'desc') ? 1 : -1;
-    return isReversed * a.name.localeCompare(b.name)
-  })
-}
-
-//Click into individual coin performance data
-<Link
-  to={(
-    pathname: '../../../tokenInformation/TokenInformation',
-    state: { datarow, token, name, USDperUnit }
-  )}
->
-
-render() {
-  const { state } = this.props.const location
-  return {
-
-  }
-    },
-  })
 }
