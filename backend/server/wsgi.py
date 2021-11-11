04e96b67560cf029cddb3281fb0f4f635fcc1af1
EMDevelop <edwardmartin.inbox@gmail.com>
2021-11-11 22:55:37 +0000

    v26

--- backend/server/wsgi.py
+++ backend/server/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+print('yooooooooooo')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
-
+print('duuudeee')
 application = get_wsgi_application()
