7013d4e7ce376657d24dda9cab33c89efc37e42f
Tom Lee <tomalee2002@gmail.com>
2021-11-11 23:07:01 +0000

    implemented universal design into popup

--- frontend/src/components/popup/popup.js
+++ frontend/src/components/popup/popup.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './_popup.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 
 function Popup(props) {
   return props.trigger ? (
     <div className="popup">
       <div className="popup-inner">
-        <button
-          className="close-button"
-          onClick={() => props.setTrigger(false)}
-        >
-          Close
-        </button>
+        <a onClick={() => props.setTrigger(false)}>
+          <div className="close-button">
+            <FontAwesomeIcon icon={faTimesCircle} />
+          </div>
+        </a>
         {props.children}
       </div>
     </div>
