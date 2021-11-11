e68f827183a81904115325b54f2dd9ccf82ccde2
Tom Lee <tomalee2002@gmail.com>
2021-11-11 21:25:09 +0000

    creates popup func component

--- frontend/src/components/popup/popup.js
+++ frontend/src/components/popup/popup.js
@@ -1,2 +1,22 @@
 import React from 'react';
 import './_popup.scss';
+
+function Popup(props) {
+  return props.trigger ? (
+    <div className="popup">
+      <div className="popup-inner">
+        <button
+          className="close-button"
+          onClick={() => props.setTrigger(false)}
+        >
+          Close
+        </button>
+        {props.children}
+      </div>
+    </div>
+  ) : (
+    ''
+  );
+}
+
+export default Popup;
